3ee6c524045b2faf3f439fca813beaac245fa8bc
marianaapolaya <apolayaschradermariana@gmail.com>
2025-08-22 12:55:42 -0500

    Avance de estructura

---
+++ app/Config/Database.php
@@ -0,0 +1,6 @@
+database.default.hostname = localhost
+database.default.database = cotizaciones_productos
+database.default.username = root
+database.default.password = 
+database.default.DBDriver = MySQLi
+database.default.DBPrefix =
